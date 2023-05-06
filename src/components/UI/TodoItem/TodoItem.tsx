ba0432ff2da0aadfbd9ea7fd4d9505d6ef49ebf8
Tim Specter <timurbadalbaev19@gmail.com>
2023-05-07 00:12:12 +0300

    feat add list

---
+++ src/components/UI/TodoItem/TodoItem.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from './todoitem.module.scss';
+import classNames from 'classnames';
+
+interface ITodoItems {
+  title: string,
+  colorId: number,
+  isActive: boolean,
+}
+
+export function TodoItem({title, colorId, isActive}: ITodoItems) {
+
+  const classItem = classNames(
+    styles.item,
+    isActive ? '' : styles.done,
+  )
+  console.log(colorId);
+  return (
+    <div className={classItem}>
+      <div className={styles[`color-${colorId}`]} />
+      <span>{title}</span>
+    </div>
+  );
+}
